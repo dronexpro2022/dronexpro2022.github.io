d3d0ce610c8ee91c3c3a2be866d2a432c4e56471
joscss <107424448+joscss@users.noreply.github.com>
2022-06-28 14:53:10 +0800

    Create canvaprolink.js

---
+++ canvaprolink.js
@@ -0,0 +1,6 @@
+document.write('<link href=\'https://joscss.github.io/button.css\' rel=\'stylesheet\'/>');
+document.write('');
+document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
+document.write('');
+document.write('<h2 style="text-align: center;"><a class=\'myButton\' href="https://wow.link/directlinkcanvaprofree" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i>');
+document.write(' Get Caneva Pro Free</a></h2>');
