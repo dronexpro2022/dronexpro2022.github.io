973e3668704c4cb2443e7c22659cc9e519fbbe04
joscss <107424448+joscss@users.noreply.github.com>
2022-07-10 13:51:07 -0500

    Update canvaprolink.js

--- canvaprolink.js
+++ canvaprolink.js
@@ -1,6 +1,11 @@
-document.writeln("<link href=\'https://joscss.github.io/button.css\' rel=\'stylesheet\'/>");
-document.writeln("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">");
-document.writeln("");
-document.writeln("<h2 style=\"text-align: center;\">");
-document.writeln("<a class=\'myButton\'href=\"https://wow.link/directlinkcanvaprofree\" target=\"_blank\"><i class=\"fa fa-external-link\" aria-hidden=\"true\"></i>Get Caneva Pro Free</a></h2>");
-document.writeln("<p style=\"text-align: center;\"><span style=\"background-color: #ffff00;\">Sometimes links do not work properly, if you think it errors then try again later. thank you! </span></p>");
+document.write('<link href=\'https://joscss.github.io/button.css\' rel=\'stylesheet\'/>');
+document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
+document.write('');
+document.write('<h2 style="text-align: center;">Link:-1');
+document.write('<a class=\'myButton\'href="https://wow.link/directlinkcanvaprofree" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i>Get Caneva Pro Free</a></h2>');
+document.write('');
+document.write('<h2 style="text-align: center;">Link:-2');
+document.write('<a class=\'myButton\'href="https://wow.link/directlinkcanvaprofree" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i>Get Caneva Pro Free</a></h2>');
+document.write('');
+document.write('<p style="text-align: center;"><span style="background-color: #ffff00;">Sometimes links do not work properly, if you think it errors then try again later. thank you! </span></p>');
+document.write('');
